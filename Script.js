7d40ae0041293fdbc3501b1b31c70a0f164c6ba2
suparwin-official <gorommasalahitbd@gmail.com>
2026-04-30 21:34:46 +0600

    Delete Script.js

--- Script.js
+++
@@ -1,9 +0,0 @@
-<input id="name" placeholder="Name">
-<input id="email" placeholder="Email">
-<input id="phone" placeholder="Phone">
-<input id="whatsapp" placeholder="WhatsApp">
-<input id="telegram" placeholder="Telegram">
-<input id="user" placeholder="Username">
-<input id="pass" type="password" placeholder="Password">
-
-<button onclick="register()">Create Account</button>
