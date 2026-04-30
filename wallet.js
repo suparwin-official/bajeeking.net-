84178564d5298ea3edcb8b87a37cd32fe31f8151
suparwin-official <gorommasalahitbd@gmail.com>
2026-04-30 21:29:48 +0600

    Update wallet.js

--- wallet.js
+++ wallet.js
@@ -1,1 +1,23 @@
+let balance = 0;
 
+function update(){
+  document.getElementById("balance").innerText = balance;
+}
+
+function deposit(){
+  let amt = Number(document.getElementById("dep").value);
+  if(amt > 0){
+    balance += amt;
+    update();
+  }
+}
+
+function withdraw(){
+  let amt = Number(document.getElementById("wit").value);
+  if(amt <= balance){
+    balance -= amt;
+    update();
+  } else {
+    alert("Not enough balance");
+  }
+}
