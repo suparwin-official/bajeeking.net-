37fc86001b7e21c71095619b656b025d07bfcd3b
suparwin-official <gorommasalahitbd@gmail.com>
2026-04-30 21:38:03 +0600

    Update User dashboard .js

--- User dashboard .js
+++ User dashboard .js
@@ -1,1 +1,11 @@
+let user = JSON.parse(localStorage.getItem("bajeeking_user"));
 
+if(user){
+  document.write(`
+    <h2>Welcome ${user.name}</h2>
+    <p>Email: ${user.email}</p>
+    <p>Phone: ${user.phone}</p>
+    <p>WhatsApp: ${user.whatsapp}</p>
+    <p>Telegram: ${user.telegram}</p>
+  `);
+}
